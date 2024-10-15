e021a8468e95578626a8c336e3eaa4e0fbc9d996
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2024-10-14 21:39:50 -0300

    fix: change telescope live grep keybinds

--- lua/rossi/plugins/telescope.lua
+++ lua/rossi/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Open file search"})
 		-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set("n", "<leader>fs", function()
+		vim.keymap.set("n", "<leader>fg", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end, { desc = "Open word search"})
 	end,
